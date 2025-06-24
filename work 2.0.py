8663e0414dd0ac7bec1b29aea59ff567279e7df9
Mwesige arnold <arnoldzender234@gmail.com>
2025-06-24 01:40:43 -0500

    Add files via upload

---
+++ work 2.0.py
@@ -0,0 +1,9 @@
+Arnold_Age = 10
+Age_at_Kindergarten = 5
+
+if Arnold_Age < Age_at_Kindergarten: 
+    print("Arnold should be in pre-school")         
+elif Arnold_Age == Age_at_Kindergarten:
+    print("Enjoy kindergarten")
+else:
+    print("Arnold should be in another class")
